4e0ec2f4a63577cf274e9dff19d6b8e3e74e818b
Jure Pustoslemšek <jure.poglednik@gmail.com>
2022-05-14 17:35:00 +0200

    file sending

--- js/sockets.js
+++ js/sockets.js
@@ -1,20 +1,28 @@
-$(document).ready = function () {
-    var socket = WebSocket("ws://localhost:3010");
-    
-    // Connection opened
-    socket.addEventListener('open', function (event) {
-        socket.send('Socket opened');
-    });
-    
-    // Listen for messages
-    socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
-    });
-    
-    fileInput = $("#fileInput");
-    sendButton = $("#sendButton");
-    sendButton.onclick = function (event) {
-        
-    }
-}
+
+var socket = new WebSocket("ws://localhost:3010");
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Socket opened');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+var fileInput = $("#fileInput");
+var sendButton = $("#sendButton");
+sendButton.click(function (event) {
+    console.log("Send button clicked");
+    var fr = new FileReader();
+    var file = fileInput.prop('files')[0];
+    fr.readAsDataURL(file);
+    fr.onload = function () {
+        var data = fr.result;
+        console.log("Sending file through socket");
+        socket.send(data);
+    } 
+})
+
 
