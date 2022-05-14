1650bf9901c5b4dea1605ecf02570ac5928aa08c
JureTic <tic.jur@gmail.com>
2022-05-14 21:03:53 +0200

    mohito dodan

--- public/recognition.js
+++ public/recognition.js
@@ -8,18 +8,21 @@ function speechToEmotion() {
   recognition.onresult = function(event) {
     const results = event.results
     const transcript = results[results.length-1][0].transcript
-
+    //searching
     setEmoji('searching')
 
     fetch(`/emotion?text=${transcript}`)
       .then((response) => response.json())
       .then((result) => {
         if (result.score > 0) {
-          setEmoji('positive')
+          //positive
+          setEmoji('mohito')
         } else if (result.score < 0) {
-          setEmoji('negative')
+          //negative
+          setEmoji('bm')
         } else {
-          setEmoji('neutral')
+          //neutral
+          setEmoji('glass')
         }
       })
       .catch((e) => {
@@ -27,18 +30,18 @@ function speechToEmotion() {
         recognition.abort()
       })
   }
-
+//error
   recognition.onerror = function(event) {
     console.error('Recognition error -> ', event.error)
-    setEmoji('error')
+    setEmoji('glass')
   }
-
+//listening
   recognition.onaudiostart = function() {
-    setEmoji('listening')
+    setEmoji('glass')
   }
-
+//idle
   recognition.onend = function() {
-    setEmoji('idle')
+    setEmoji('glass')
   }
 
   recognition.start();
