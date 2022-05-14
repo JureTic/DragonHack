a4f201c54930454d8ac18c8fdf51365ee28d2d6c
FooDawn <mal.medo987@gmail.com>
2022-05-14 15:25:45 +0200

    init py

---
+++ feelings.py
@@ -0,0 +1,13 @@
+
+class Speach_feelings:
+    def __init__(self) -> None:
+        self.feelings = ["mad", "sad", "happy", "meh", "frijevec", "extatic"]
+        
+
+    def get_feeling(self):
+        """ Return one of predetermind feelings. """
+        pass
+
+
+if __name__ == "__main__":
+    Speach_feelings()
